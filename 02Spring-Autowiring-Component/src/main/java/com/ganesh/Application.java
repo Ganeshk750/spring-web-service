6bca91c0b96f373185702614def0e41365d68a2f
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-08-03 16:17:47 +0530

    # done

--- 02Spring-Autowiring-Component/src/main/java/com/ganesh/Application.java
+++ 02Spring-Autowiring-Component/src/main/java/com/ganesh/Application.java
@@ -6,7 +6,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.ganesh.beans")
 public class Application {
 
 	public static void main(String[] args) {
